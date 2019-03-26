e86cb22b4916c8c27f0caaf58be77905e909a7d6
frealis <36343653+frealis@users.noreply.github.com>
2019-03-26 11:23:22 -0400

    Create Message class in models.py, add extension field to phone number, change message <input> to <textarea>

--- web_app/models.py
+++ web_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+  name = models.CharField(max_length=64, default=None)
+  address = models.CharField(max_length=200, default=None)
+  phone = models.CharField(max_length = 32, default=None)
+  email = models.CharField(max_length = 64, default=None)
+  message = models.CharField(max_length = 800, default=None)
+  created = models.DateTimeField(auto_now_add=True)
+  def __str__(self):
+    return f"name: {self.name}, address: {self.address}, phone: {self.phone}, email: {self.email}, message: {self.message}, created: {self.created}"
+
+
+
+    
--- web_app/views.py
+++ web_app/views.py
@@ -6,4 +6,12 @@ def index(request):
   return render(request, 'web_app/index.html')
 
 def test(request):
+  if request.method == 'POST':
+    name = request.POST['name']
+    address = request.POST['address']
+    phone = request.POST['phone_ac']
+    email = request.POST['email']
+    message = request.POST['message']
+
+
   return render(request, 'web_app/test.html')
