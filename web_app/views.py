257a3a33ebc11d8e409a03e58c1b8d9d2ea3d23c
frealis <36343653+frealis@users.noreply.github.com>
2019-03-28 13:11:59 -0400

    Update 'Contact Us' form, send notification to site admin and thank you email to user after form submission

--- web_app/views.py
+++ web_app/views.py
@@ -1,5 +1,7 @@
+from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.shortcuts import render
+import os
 
 # Create your views here.
 def index(request):
@@ -7,6 +9,8 @@ def index(request):
 
 def test(request):
   if request.method == 'POST':
+
+    # Gather data submitted from the "Contact Us" form
     name = request.POST['name']
     address = request.POST['address']
     phone = request.POST['phone']
@@ -15,8 +19,31 @@ def test(request):
 
     # print('name: ', name, 'address: ', address, 'phone: ', phone, 'email: ', email, 'message: ', message)
 
+    # Save data submitted from the "Contact Us" form to sqlite3 database
     from web_app.models import Message
     m = Message(name=name, address=address, phone=phone, email=email, message=message)
     m.save()
+
+    # Set the email address for the site administrator
+    email_admin = os.getenv("EMAIL_ADMIN")
+
+    # Send a notification message to the site administrator when "Contact Us" form
+    # is submitted
+    send_mail(
+      'River City Pro Wash -- Contact Us form submission notification',
+      'Name: {}\nAddress: {}\nPhone: {}\nEmail: {}\nMessage: {}'.format(name, address, phone, email, message),
+      email_admin,
+      [email_admin],
+      fail_silently=False,
+    )
+
+    # Send a thank you message to the user who submitted the "Contact Us" form
+    send_mail(
+      'Thank you for contacting River City Pro Wash!',
+      'Dear {},\n\nThank you for contacting River City Pro Wash! A member of our team will be in touch with you shortly.\n\nRegards,\nRiver City Pro Wash'.format(name),
+      email_admin,
+      [email],
+      fail_silently=False,
+    )
 
   return render(request, 'web_app/test.html')
