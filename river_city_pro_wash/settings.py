0120d2186f57db90080eebb8712e241f3f7de504
frealis <36343653+frealis@users.noreply.github.com>
2019-04-04 16:00:20 -0400

    Pre-heroku deployment

--- river_city_pro_wash/settings.py
+++ river_city_pro_wash/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.99.100', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [] # ---- Figure this out with AWS or Heroku or whoever
 
 
 # Application definition
