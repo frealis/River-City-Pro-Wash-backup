1a01d234ba40e5d71eaa027999c02f54c0ff0dcb
frealis <36343653+frealis@users.noreply.github.com>
2019-04-06 11:54:36 -0400

    Add Procfile, Procfile.windows, and gunicorn

--- river_city_pro_wash/settings.py
+++ river_city_pro_wash/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [] # ---- Figure this out with AWS or Heroku or whoever
+ALLOWED_HOSTS = ['*'] # ---- Figure this out with AWS or Heroku or whoever
 
 
 # Application definition
