76c9b8a4a51130f46e4a38f824b31249bd02f3f8
frealis <36343653+frealis@users.noreply.github.com>
2019-04-07 10:03:15 -0400

    Require SSL

--- river_city_pro_wash/settings.py
+++ river_city_pro_wash/settings.py
@@ -30,7 +30,7 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 X_FRAME_OPTIONS = 'DENY'
@@ -42,8 +42,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['frozen-brushlands-44280.herokuapp.com']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['frozen-brushlands-44280.herokuapp.com']
+# ALLOWED_HOSTS = ['*']
 
 # Application definition
 
