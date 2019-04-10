26605b1149c1b91ab02b0148771d72b75d89ebf0
frealis <36343653+frealis@users.noreply.github.com>
2019-04-10 00:27:18 -0400

    settings.py production

--- river_city_pro_wash/settings.py
+++ river_city_pro_wash/settings.py
@@ -186,35 +186,35 @@ EMAIL_USE_SSL = True
 django_heroku.settings(locals())
 
 # Logging
-LOGGING = {
-  'version': 1,
-  'disable_existing_loggers': False,
-  'formatters': {
-    'verbose': {
-      'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-      'datefmt' : "%d/%b/%Y %H:%M:%S"
-    },
-    'simple': {
-      'format': '%(levelname)s %(message)s'
-    },
-  },
-  'handlers': {
-    'file': {
-      'level': 'DEBUG',
-      'class': 'logging.FileHandler',
-      'filename': 'river_city_pro_wash.log',
-      'formatter': 'verbose'
-    },
-  },
-  'loggers': {
-    'django': {
-      'handlers':['file'],
-      'propagate': True,
-      'level':'DEBUG',
-    },
-    'MYAPP': {
-      'handlers': ['file'],
-      'level': 'DEBUG',
-    },
-  }
-}
+# LOGGING = {
+#   'version': 1,
+#   'disable_existing_loggers': False,
+#   'formatters': {
+#     'verbose': {
+#       'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+#       'datefmt' : "%d/%b/%Y %H:%M:%S"
+#     },
+#     'simple': {
+#       'format': '%(levelname)s %(message)s'
+#     },
+#   },
+#   'handlers': {
+#     'file': {
+#       'level': 'DEBUG',
+#       'class': 'logging.FileHandler',
+#       'filename': 'river_city_pro_wash.log',
+#       'formatter': 'verbose'
+#     },
+#   },
+#   'loggers': {
+#     'django': {
+#       'handlers':['file'],
+#       'propagate': True,
+#       'level':'DEBUG',
+#     },
+#     'MYAPP': {
+#       'handlers': ['file'],
+#       'level': 'DEBUG',
+#     },
+#   }
+# }
