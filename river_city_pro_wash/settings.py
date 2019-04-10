900a85050034bdbdab6c3f3db37a13d8ad35d11c
frealis <36343653+frealis@users.noreply.github.com>
2019-04-09 23:08:03 -0400

    Adjust settings.py

--- river_city_pro_wash/settings.py
+++ river_city_pro_wash/settings.py
@@ -40,7 +40,7 @@ X_FRAME_OPTIONS = 'DENY'
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 SECURE_SSL_REDIRECT = True
 ALLOWED_HOSTS = ['frozen-brushlands-44280.herokuapp.com']
