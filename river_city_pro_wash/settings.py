6deeac4bc0b518b23e3d87d48ece6d7ab26ebdb0
frealis <36343653+frealis@users.noreply.github.com>
2019-04-11 09:28:15 -0400

    Developer settings

--- river_city_pro_wash/settings.py
+++ river_city_pro_wash/settings.py
@@ -26,14 +26,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-CSRF_COOKIE_SECURE = True
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_SECONDS = 518400
-SESSION_COOKIE_SECURE = True
-X_FRAME_OPTIONS = 'DENY'
+# CSRF_COOKIE_SECURE = True
+# SECURE_BROWSER_XSS_FILTER = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
+# SECURE_HSTS_SECONDS = 518400
+# SESSION_COOKIE_SECURE = True
+# X_FRAME_OPTIONS = 'DENY'
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -42,12 +42,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production! -- If you set
 # this to FALSE, make sure you have an appropriate allowed site listed in the
 # ALLOWED_HOSTS[] array below.
-DEBUG = False
+DEBUG = True
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
-SECURE_SSL_REDIRECT = True
-ALLOWED_HOSTS = ['https://frozen-brushlands-44280.herokuapp.com/']
-# ALLOWED_HOSTS = ['*']
+# SECURE_SSL_REDIRECT = True
+# ALLOWED_HOSTS = ['https://frozen-brushlands-44280.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
