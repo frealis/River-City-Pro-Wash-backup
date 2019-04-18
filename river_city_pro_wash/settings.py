8f80759348efc4d720295b1e6f04e58d315c6b45
frealis <36343653+frealis@users.noreply.github.com>
2019-04-18 14:11:11 -0400

    Disable Gmail smtp settings

--- river_city_pro_wash/settings.py
+++ river_city_pro_wash/settings.py
@@ -174,13 +174,13 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-# Email
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+# Email settings for Gmail
+# EMAIL_HOST = os.getenv("EMAIL_HOST")
+# EMAIL_PORT = os.getenv("EMAIL_PORT")
+# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+# EMAIL_USE_TLS = False
+# EMAIL_USE_SSL = True
 
 # Activate Django-Heroku
 # django_heroku.settings(locals())
