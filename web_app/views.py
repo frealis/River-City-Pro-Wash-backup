83c83265f6e6331ed7c63de87cd7d08e4b50f0b7
frealis <36343653+frealis@users.noreply.github.com>
2019-04-18 13:58:09 -0400

    Updated contact info/hours of operation, added 56px margin to top of carosel.html and contact_us.html to account for navbar height which is fixed so it sits on top of the DOM and everything under it needs to be corrected

--- web_app/views.py
+++ web_app/views.py
@@ -26,10 +26,10 @@ def index(request):
     # Authenticate reCAPTCHA v2 user's response
     
     # reCAPTCHA v2 SECRET key
-    RECAPTCHA_SITE_SECRET = os.getenv('RECAPTCHA_SITE_SECRET')
+    # RECAPTCHA_SITE_SECRET = os.getenv('RECAPTCHA_SITE_SECRET')
 
     # reCAPTCHA v2 SECRET key, test
-    # RECAPTCHA_SITE_SECRET = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+    RECAPTCHA_SITE_SECRET = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
 
     a = os.getenv('RECAPTCHA_SITE_VERIFY_URL')
     b = urllib.parse.urlencode({'secret': RECAPTCHA_SITE_SECRET, 'response': request.POST['recaptcha']}, True)
@@ -70,8 +70,8 @@ def index(request):
       message = Mail(
         from_email=email_admin,
         to_emails=email_admin,
-        subject='Thank you for contacting River City Pro Wash!',
-        html_content=Template('Name: $name\nAddress: $address\nPhone: $phone\nEmail: $email\nMessage: $message').substitute(name=name, address=address, phone=phone, email=email, message=message))
+        subject='River City Pro Wash Contact Form Submission',
+        html_content=Template('Name: $name<br>Address: $address<br>Phone: $phone<br>Email: $email<br>Message: $message').substitute(name=name, address=address, phone=phone, email=email, message=message))
       try:
         sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
         response = sg.send(message)
@@ -83,7 +83,7 @@ def index(request):
         from_email=email_admin,
         to_emails=email,
         subject='Thank you for contacting River City Pro Wash!',
-        html_content=Template('Dear $name,\n\nThank you for contacting River City Pro Wash! A member of our team will be in touch with you shortly.\n\nRegards,\nRiver City Pro Wash').substitute(name=name))
+        html_content=Template('Dear $name,<br><br>Thank you for contacting River City Pro Wash! A member of our team will be in touch with you shortly.<br><br>Regards,<br>River City Pro Wash<br><br>').substitute(name=name))
       try:
         sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
         response = sg.send(message)
