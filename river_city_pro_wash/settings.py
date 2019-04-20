dcdcbb1e5c55fad0befd37f6aab7e6ed38959ad8
frealis <36343653+frealis@users.noreply.github.com>
2019-04-19 22:48:51 -0400

    Enable sqlite3

--- river_city_pro_wash/settings.py
+++ river_city_pro_wash/settings.py
@@ -100,12 +100,12 @@ WSGI_APPLICATION = 'river_city_pro_wash.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Dockerfile database
 # DATABASES = {
