464386c8604acb17e71e2348f7d1ec2db51a07ff
frealis <36343653+frealis@users.noreply.github.com>
2019-04-21 09:51:46 -0400

    settings.py

--- river_city_pro_wash/settings.py
+++ river_city_pro_wash/settings.py
@@ -35,20 +35,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SESSION_COOKIE_SECURE = True
 # X_FRAME_OPTIONS = 'DENY'
 
-CORS_REPLACE_HTTPS_REFERER      = True
-HOST_SCHEME                     = "https://"
-SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT             = True
-SESSION_COOKIE_SECURE           = True
-CSRF_COOKIE_SECURE              = True
-SECURE_HSTS_PRELOAD             = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
-SECURE_HSTS_SECONDS             = 1000000
-SECURE_FRAME_DENY               = True
+# CORS_REPLACE_HTTPS_REFERER      = True
+# HOST_SCHEME                     = "https://"
+# SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT             = True
+# SESSION_COOKIE_SECURE           = True
+# CSRF_COOKIE_SECURE              = True
+# SECURE_HSTS_PRELOAD             = True
+# SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+# SECURE_HSTS_SECONDS             = 1000000
+# SECURE_FRAME_DENY               = True
 
-SECURE_CONTENT_TYPE_NOSNIFF     = True
-SECURE_BROWSER_XSS_FILTER       = True
-X_FRAME_OPTIONS                 = 'DENY'
+# SECURE_CONTENT_TYPE_NOSNIFF     = True
+# SECURE_BROWSER_XSS_FILTER       = True
+# X_FRAME_OPTIONS                 = 'DENY'
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
