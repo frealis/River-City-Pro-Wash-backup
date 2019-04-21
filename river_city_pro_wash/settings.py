b4e75f3d39fabde7e89636ed491d77179168d858
frealis <36343653+frealis@users.noreply.github.com>
2019-04-20 23:41:39 -0400

    settings.py

--- river_city_pro_wash/settings.py
+++ river_city_pro_wash/settings.py
@@ -38,7 +38,7 @@ X_FRAME_OPTIONS = 'DENY'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.getenv("SECRET_KEY")    # heroku
-SECRET_KEY = os.environ["SECRET_KEY"]   # aws
+# SECRET_KEY = os.environ["SECRET_KEY"]   # aws
 
 
 # SECURITY WARNING: don't run with debug turned on in production! -- If you set
