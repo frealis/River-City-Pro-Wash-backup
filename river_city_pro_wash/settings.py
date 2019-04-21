afa3713b578f90a7d5e907cafb907973c3a6244b
frealis <36343653+frealis@users.noreply.github.com>
2019-04-20 20:13:29 -0400

    Update README.md, update secret key

--- river_city_pro_wash/settings.py
+++ river_city_pro_wash/settings.py
@@ -16,8 +16,8 @@ import os
 
 # Use python-dotenv to manage environment variables
 # https://github.com/theskumar/python-dotenv
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,7 +37,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ["SECRET_KEY"]
 # SECRET_KEY = 'kms#@8-n4_l$)s4)8z4zo-70l-j$3+y+w7d0wdy%184m8i2684'
 
 
