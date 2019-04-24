580c0459fb24d7a7d05b132f8786af9da8271ad3
frealis <36343653+frealis@users.noreply.github.com>
2019-04-23 21:18:07 -0400

    AWS production settings

--- web_app/views.py
+++ web_app/views.py
@@ -56,8 +56,8 @@ def index(request):
       RECIPIENT = Template("$email").substitute(email=email)
       AWS_REGION = "us-east-1"
 
-      print('=== SENDER: ')
-      print('=== RECIPIENT: ')
+      print('=== SENDER: ', SENDER)
+      print('=== RECIPIENT: ', RECIPIENT)
 
       # The subject line for the email.
       SUBJECT = "Amazon SES Test (SDK for Python)"
