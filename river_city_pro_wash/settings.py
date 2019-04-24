ae315682f09fbf4fe2e5ac398702ad77e87cb94a
frealis <36343653+frealis@users.noreply.github.com>
2019-04-24 12:56:55 -0400

    Update AWS Postgres settings

--- river_city_pro_wash/settings.py
+++ river_city_pro_wash/settings.py
@@ -106,35 +106,17 @@ WSGI_APPLICATION = 'river_city_pro_wash.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# Heroku, Dockerfile Postgres database
+# AWS Postgres database
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'postgres',
-    'USER': 'postgres',
-    'HOST': 'db',
+    'NAME': os.environ['AWS_POSTGRES_NAME'],
+    'USER': os.environ['AWS_POSTGRES_USER'],
+    'PASSWORD': os.environ['AWS_POSTGRES_PASSWORD'],
+    'HOST': os.environ['AWS_POSTGRES_HOST'],
     'PORT': '5432',
   }
 }
-
-# Local Postgres database
-# DATABASES = {
-#   'default': {
-#     'ENGINE': 'django.db.backends.postgresql',
-#     'NAME': os.getenv("POSTGRES_NAME"),
-#     'USER': os.getenv("POSTGRES_USER"),
-#     'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-#     'HOST': '127.0.0.1',
-#     'PORT': '5432',
-#   }
-# }
 
 # Un-comment this if you want to run python manage.py check --deploy to 2x check
 # that security settings are in place before deployment without worrying about
