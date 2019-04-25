5a19d2b031433ac9e9db5ce23623fef400e4ee47
frealis <36343653+frealis@users.noreply.github.com>
2019-04-25 12:58:58 -0400

    db

--- river_city_pro_wash/settings.py
+++ river_city_pro_wash/settings.py
@@ -181,35 +181,35 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static') # aws
 
 
 # Logging
-LOGGING = {
-  'version': 1,
-  'disable_existing_loggers': False,
-  'formatters': {
-    'verbose': {
-      'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-      'datefmt' : "%d/%b/%Y %H:%M:%S"
-    },
-    'simple': {
-      'format': '%(levelname)s %(message)s'
-    },
-  },
-  'handlers': {
-    'file': {
-      'level': 'DEBUG',
-      'class': 'logging.FileHandler',
-      'filename': 'project.log',
-      'formatter': 'verbose'
-    },
-  },
-  'loggers': {
-    'django': {
-      'handlers':['file'],
-      'propagate': True,
-      'level':'DEBUG',
-    },
-    'MYAPP': {
-      'handlers': ['file'],
-      'level': 'DEBUG',
-    },
-  }
-}
+# LOGGING = {
+#   'version': 1,
+#   'disable_existing_loggers': False,
+#   'formatters': {
+#     'verbose': {
+#       'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+#       'datefmt' : "%d/%b/%Y %H:%M:%S"
+#     },
+#     'simple': {
+#       'format': '%(levelname)s %(message)s'
+#     },
+#   },
+#   'handlers': {
+#     'file': {
+#       'level': 'DEBUG',
+#       'class': 'logging.FileHandler',
+#       'filename': 'project.log',
+#       'formatter': 'verbose'
+#     },
+#   },
+#   'loggers': {
+#     'django': {
+#       'handlers':['file'],
+#       'propagate': True,
+#       'level':'DEBUG',
+#     },
+#     'MYAPP': {
+#       'handlers': ['file'],
+#       'level': 'DEBUG',
+#     },
+#   }
+# }
