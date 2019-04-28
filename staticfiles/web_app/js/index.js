bad2d3c12605cf290630c12cea6afb4050312420
frealis <36343653+frealis@users.noreply.github.com>
2019-04-28 14:11:42 -0400

    collectstatic

--- staticfiles/web_app/js/index.js
+++ staticfiles/web_app/js/index.js
@@ -72,94 +72,85 @@ document.addEventListener('DOMContentLoaded', function() {
   // Retrieve message from "Contact Us" form and submit
   document.querySelector('#send_message').onclick = () => {
 
-    // console.log('grecaptcha.getResponse(): ', grecaptcha.getResponse())
+    alert('click');
 
-    // Ensure that the reCAPTCHA v2 checkbox is ticked
-    if (grecaptcha.getResponse() !== '') {
+    // Initialize POST request, extract the CSRF value from the index.html DOM,
+    // and put that into the header of the POST request
+    const request = new XMLHttpRequest();
+    const csrf_token = document.querySelector('#csrf').childNodes[0]['value'];
+    request.open('POST', '/');
+    request.setRequestHeader("X-CSRFToken", csrf_token);
 
-      // Initialize POST request, extract the CSRF value from the index.html DOM,
-      // and put that into the header of the POST request
-      const request = new XMLHttpRequest();
-      const csrf_token = document.querySelector('#csrf').childNodes[0]['value'];
-      request.open('POST', '/');
-      request.setRequestHeader("X-CSRFToken", csrf_token);
+    // Retrieve user input from the "Contact Us" form
+    let name      = document.querySelector('#name').value;
+    let address   = document.querySelector('#address').value;
+    let phone_ac  = document.querySelector('#phone_ac').value;
+    let phone_3d  = document.querySelector('#phone_3d').value;
+    let phone_4d  = document.querySelector('#phone_4d').value;
+    let phone     = phone_ac + phone_3d + phone_4d
+    if (document.querySelector('#phone_ext').value !== '') {
+      let phone_ext = document.querySelector('#phone_ext').value;
+      phone += ' ext. ' + phone_ext;
+    };
+    let email     = document.querySelector('#email').value;
+    let message   = document.querySelector('#message').value;
 
-      // Retrieve user input from the "Contact Us" form
-      let name      = document.querySelector('#name').value;
-      let address   = document.querySelector('#address').value;
-      let phone_ac  = document.querySelector('#phone_ac').value;
-      let phone_3d  = document.querySelector('#phone_3d').value;
-      let phone_4d  = document.querySelector('#phone_4d').value;
-      let phone     = phone_ac + phone_3d + phone_4d
-      if (document.querySelector('#phone_ext').value !== '') {
-        let phone_ext = document.querySelector('#phone_ext').value;
-        phone += ' ext. ' + phone_ext;
+    // Ensure that the Name, Email Address, and Message fields are not empty in
+    // the "Contact Us" form
+    if (name !== '' && email !== '' && message !== '') {
+
+      // Clear "Contact Us" form data after submission
+      document.querySelector('#name').value = '';
+      document.querySelector('#address').value = '';
+      document.querySelector('#phone_ac').value = '';
+      document.querySelector('#phone_3d').value = '';
+      document.querySelector('#phone_4d').value = '';
+      document.querySelector('#email').value = '';
+      document.querySelector('#message').value = '';
+
+      // Clear the error message and revert required field headers to default 
+      // color in case the user had previously submitted an incomplete form.
+      document.querySelector('#alert').innerHTML = "";
+      document.querySelector('#alert').className = "";
+      required_fields = document.querySelectorAll('#required_field');
+      for (let i = 0; i < required_fields.length; i++) {
+        required_fields[i].style.color="black";
       };
-      let email     = document.querySelector('#email').value;
-      let message   = document.querySelector('#message').value;
 
-      // Ensure that the Name, Email Address, and Message fields are not empty in
-      // the "Contact Us" form
-      if (name !== '' && email !== '' && message !== '') {
+      // Append user input & reCAPTCHA v3 token to message_data (the FormData() 
+      // object)
+      const message_data = new FormData();
+      message_data.append('name',       name);
+      message_data.append('address',    address);
+      message_data.append('phone',      phone);
+      message_data.append('email',      email);
+      message_data.append('message',    message);
+      message_data.append('recaptcha',  'recaptcha');
 
-        // Clear "Contact Us" form data after submission
-        document.querySelector('#name').value = '';
-        document.querySelector('#address').value = '';
-        document.querySelector('#phone_ac').value = '';
-        document.querySelector('#phone_3d').value = '';
-        document.querySelector('#phone_4d').value = '';
-        document.querySelector('#email').value = '';
-        document.querySelector('#message').value = '';
+      // View the contents of FormData() in browser console using the strange
+      // *.entries() method.
+      // for (let i of message_data.entries()) {
+      //   console.log(i);
+      // };
 
-        // Clear the error message and revert required field headers to default 
-        // color in case the user had previously submitted an incomplete form.
-        document.querySelector('#alert').innerHTML = "";
-        document.querySelector('#alert').className = "";
-        required_fields = document.querySelectorAll('#required_field');
-        for (let i = 0; i < required_fields.length; i++) {
-          required_fields[i].style.color="black";
-        };
+      // Send a "Message successfully sent!" alert to the DOM in the event of a
+      // successful "Contact Us" form submission.
+      document.querySelector('#alert').innerHTML = "Your message has been sent!";
+      document.querySelector('#alert').className = "alert alert-success";
 
-        // Append user input & reCAPTCHA v2 token to message_data (the FormData() 
-        // object).
-        const message_data = new FormData();
-        message_data.append('name',       name);
-        message_data.append('address',    address);
-        message_data.append('phone',      phone);
-        message_data.append('email',      email);
-        message_data.append('message',    message);
-        message_data.append('recaptcha',  grecaptcha.getResponse());
-        // message_data.append('recaptcha',  grecaptcha.getResponse);
+      // Send FormData() object to server
+      request.send(message_data);
+      return false;
 
-        // View the contents of FormData() in browser console using the strange
-        // *.entries() method.
-        // for (let i of message_data.entries()) {
-        //   console.log(i);
-        // };
-
-        // Send a "Message successfully sent!" alert to the DOM in the event of a
-        // successful "Contact Us" form submission.
-        document.querySelector('#alert').innerHTML = "Your message has been sent!";
-        document.querySelector('#alert').className = "alert alert-success";
-
-        // Send FormData() object to server
-        request.send(message_data);
-        return false;
-
-      // Display error message and highlight required fields if any of them are
-      // empty
-      } else {
-        document.querySelector('#alert').innerHTML = "Please fill in all required fields.";
-        document.querySelector('#alert').className = "alert alert-danger";
-        required_fields = document.querySelectorAll('.required_field');
-        for (let i = 0; i < required_fields.length; i++) {
-          required_fields[i].style.color="red";
-        };
-        return false;
-      };
+    // Display error message and highlight required fields by changing the text
+    // color to red if any of them are empty
     } else {
-      document.querySelector('#alert').innerHTML = "Please click the reCAPTCHA checkbox below.";
+      document.querySelector('#alert').innerHTML = "Please fill in all required fields.";
       document.querySelector('#alert').className = "alert alert-danger";
+      required_fields = document.querySelectorAll('.required_field');
+      for (let i = 0; i < required_fields.length; i++) {
+        required_fields[i].style.color="red";
+      };
       return false;
     };
   };
