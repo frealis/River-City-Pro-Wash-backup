7c127ca8c97e04f816503184be4a457e76a0a78c
frealis <36343653+frealis@users.noreply.github.com>
2019-04-28 23:59:19 -0400

    AWS production, collectstatic

--- web_app/views.py
+++ web_app/views.py
@@ -21,17 +21,18 @@ def index(request):
     message = bleach.clean(request.POST['message'])
     ip = request.META['REMOTE_ADDR']
     
-    # reCAPTCHA v2 SECRET key
-    # RECAPTCHA_SITE_SECRET = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'  # local
+    # reCAPTCHA v3 SECRET key
     # RECAPTCHA_SITE_SECRET = os.getenv('RECAPTCHA_SITE_SECRET')    # heroku
     RECAPTCHA_SITE_SECRET = os.environ['RECAPTCHA_SITE_SECRET']   # aws
 
     # a = os.getenv('RECAPTCHA_SITE_VERIFY_URL')    # heroku, local
     a = os.environ['RECAPTCHA_SITE_VERIFY_URL']     # aws
 
-    b = urllib.parse.urlencode({'secret': RECAPTCHA_SITE_SECRET, 'response': request.POST['recaptcha']}, True)
+    b = urllib.parse.urlencode({'secret': RECAPTCHA_SITE_SECRET, 'response': request.POST['recaptcha_token']}, True)
     c = urllib.request.Request(a + '?' + b)
     recaptcha_response = urllib.request.urlopen(c).read().decode("utf-8")
+
+    print('=================== recaptcha_response: ', recaptcha_response)
     if json.loads(recaptcha_response).get("success") == True:
       recaptcha = 'Success'
       print('=== reCAPTCHA succeeded ===')
